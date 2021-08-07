2868e11d9d3979e656df14b56227b2e23fd5b5b7
bryanahusna <51874424+bryanahusna@users.noreply.github.com>
2021-08-08 01:45:29 +0700

    Menambahkan Heroku PostgreSQL
    Databases PostgreSQL untuk menyimpan riwayat chat pengguna.
    Riwayat chat pengguna selanjutnya dapat digunakan untuk dianalisis lebih lanjut.
    Format SQL
    nama tabel: percakapan
    (nama, user_id, waktu_epoch, waktu, pesan)
    NOTE: Waktu epoch berguna untuk mengurutkan waktu percakapan dengan mudah

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import psycopg2     # PostgreSQL
 from flask import Flask, request, abort
 
 from linebot import (
@@ -7,15 +8,25 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
+from datetime import datetime
 
 app = Flask(__name__)
+
+# Heroku PostgreSQL Setups
+psql_host = 'ec2-54-196-65-186.compute-1.amazonaws.com'
+psql_database = 'd12jneq73g7u2'
+psql_user = 'aaqgmutpyfxgmx'
+psql_port = 5432
+psql_password = '614e04f0ec7d6a687c0b4e8c6a9941391d70349037ac3b1384149752bd7eeacd'
+psql_uri = 'postgres://aaqgmutpyfxgmx:614e04f0ec7d6a687c0b4e8c6a9941391d70349037ac3b1384149752bd7eeacd@ec2-54-196-65-186.compute-1.amazonaws.com:5432/d12jneq73g7u2'
+psql_herokucli = 'heroku pg:psql postgresql-trapezoidal-98002 --app kirana-bot'
 
 # Channel Access Token
 line_bot_api = LineBotApi('KBYcJt1ZmbmMnkQM0ZW6uREsAtE7QSARwDrVprACm91i3/zpvlJZVHXVVFnVDuRorEceLSqwx8qV/fIDE/qpJF1wdNMykK0kgHIxEeeNywBXIKvcWp+q9Rxw1a3C661yzKWgR/8AYzMt3eLgHAj3MwdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('38cb174b5ffbf238b2b7048c47676654')
 
-# 監聽所有來自 /callback Post Request
+# callback Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -33,10 +44,28 @@ def callback():
 # Take user's sent text
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    user_id = event.source.user_id
+    user_profile = line_bot_api.get_profile(user_id=user_id)
+    
+    time_now = datetime.now()
+    pesan_sql = '({}, {}, {}-{}-{} {}:{}, {}, {})'.format(
+                user_profile.display_name, user_id, int(datetime.now().timestamp()),
+                time_now.day, time_now.month, time_now.year, time_now.hour, time_now.minute,
+                event.message.text)
+    psql_cur.execute("INSERT INTO percakapan VALUES " + pesan_sql + ';')
+
+    sent_msg = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, sent_msg)
+
 
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    psql_conn = psycopg2.connect(host=psql_host,
+                                 database=psql_database, 
+                                 user=psql_user,
+                                 password=psql_password,
+                                 port=psql_port)
+    psql_cur = psql_conn.cursor()
+    psql_cur.execute('CREATE TABLE IF NOT EXISTS percakapan (nama_user text, id_user text, waktu_epoch integer, waktu text, pesan text);')
