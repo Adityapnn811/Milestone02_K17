fc1350c2219cf362d45f03bc9bb26b1f2c1a4751
bryanahusna <51874424+bryanahusna@users.noreply.github.com>
2021-08-10 13:18:30 +0700

    Pertama mencoba berpindah mode Admin dan Bot

--- src/app.py
+++ src/app.py
@@ -13,6 +13,8 @@ import pytz
 
 app = Flask(__name__)
 
+id_admin = "U2cc53b28669cf7c907d47e8653c08c6a"
+
 # Heroku PostgreSQL Setups
 psql_riwayatcakap = 'riwayatcakap'
 psql_host = 'ec2-54-196-65-186.compute-1.amazonaws.com'
@@ -71,18 +73,36 @@ def handle_message(event):
     user_id = event.source.user_id
     user_profile = line_bot_api.get_profile(user_id=user_id)
 
-    try:
-        time_now = datetime.now(pytz.timezone('Etc/GMT-7'))
-        pesan_sql = "('{}', '{}', {}, '{}-{}-{} {:02}:{:02}', '{}')".format(
-                user_profile.display_name.replace("'", "''"), user_id, int(datetime.now().timestamp()),
-                time_now.day, time_now.month, time_now.year, time_now.hour, time_now.minute,
-                event.message.text.replace("'", "''"))
-        psql_cur.execute("INSERT INTO riwayatcakap VALUES " + pesan_sql + ";")
-        psql_conn.commit()
-        sent_msg = TextSendMessage(text=pesan_sql)
-        line_bot_api.reply_message(event.reply_token, sent_msg)
-    except:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Maaf, suatu error terjadi"))
+    psql_cur.execute("SELECT * FROM chatadmin WHERE id_user={};".format(user_id))
+    result = psql_cur.fetchone()
+    # Jika Admin dan ada client yang menghubungi admin
+    if result and user_id == id_admin:
+        id_client = result[0]
+        line_bot_api.push_message(id_client, TextSendMessage(text=event.message.text))
+    elif result:
+        id_client = result[0]
+        if "mode bot" in event.message.text.lower():
+            psql_cur.execute("DELETE FROM chatadmin WHERE id_user={};".format(id_client))
+            line_bot_api.reply_message(event.reply_token, [TextSendMessage(text="Berpindah ke Mode Bot"), TextSendMessage(text="Sekarang Anda berbicara dengan Bot")])
+        else:
+            line_bot_api.push_message(id_admin, TextSendMessage(text=event.message.text))
+    else:
+        if "mode admin" in event.message.text.lower():
+            psql_cur.execute("INSERT INTO chatadmin VALUES ({});".format(user_id))
+            line_bot_api.reply_message(event.reply_token, [TextSendMessage(text="Berpindah ke Mode Admin"), TextSendMessage(text="Sekarang Anda berbicara dengan Admin")])
+            return
+        try:
+            time_now = datetime.now(pytz.timezone('Etc/GMT-7'))
+            pesan_sql = "('{}', '{}', {}, '{}-{}-{} {:02}:{:02}', '{}')".format(
+                    user_profile.display_name.replace("'", "''"), user_id, int(datetime.now().timestamp()),
+                    time_now.day, time_now.month, time_now.year, time_now.hour, time_now.minute,
+                    event.message.text.replace("'", "''"))
+            psql_cur.execute("INSERT INTO riwayatcakap VALUES " + pesan_sql + ";")
+            psql_conn.commit()
+            sent_msg = TextSendMessage(text=pesan_sql)
+            line_bot_api.reply_message(event.reply_token, sent_msg)
+        except:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Maaf, suatu error terjadi"))
 
 import os
 if __name__ == "__main__":
