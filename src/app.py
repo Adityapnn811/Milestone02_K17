7867fb6cf12bd234d99180b7a243fcf9c77c98bf
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2021-08-14 15:11:29 +0700

    Fixing carousel

--- src/app.py
+++ src/app.py
@@ -177,14 +177,14 @@ def handle_message(event):
     if 'info' in user_msg.lower():
         carousel_template = CarouselTemplate(columns=[
             CarouselColumn(thumbnailImageUrl='https://img.okezone.com/content/2020/12/19/408/2330718/menikmati-pesona-golden-sunrise-dengan-7-puncak-gunung-di-temanggung-FF217tHxnd.jpg/240', text='Baca artikel tentang kesehatan mental sekarang!', title='Artikel Kesehatan Mental', actions=[
-                URIAction(label='Baca artikel di sini!', uri='https://www.liputan6.com/tag/kesehatan-mental?type=text&page=1')
+                URIAction(label='Baca artikel di sini!', uri='https://www.liputan6.com/health/read/4626026/5-aplikasi-virtual-untuk-atasi-stres-dan-kesehatan-mental-saat-pandemi-covid-19')
             ]),
             CarouselColumn(text='Motivasi-in kamu', title='Semangat!', actions=[
                 MessageAction(label='Motivate me!', text=Random_Motivasi())
             ]),
         ])
         template_message = TemplateSendMessage(
-            alt_text='Carousel alt text', template=carousel_template)
+            alt_text='Fitur Bot', template=carousel_template)
         line_bot_api.reply_message(event.reply_token, template_message)
 
     # Di bawah ini bagian logika percakapan pengguna
