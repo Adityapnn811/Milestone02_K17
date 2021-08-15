e6e409819e6a83bfe20e3a24083d6c9ed464c593
bryanahusna <51874424+bryanahusna@users.noreply.github.com>
2021-08-15 21:30:54 +0700

    Update mode_switch.py

--- src/mode_switch.py
+++ src/mode_switch.py
@@ -1,5 +1,5 @@
 
-id_admins = ["U2cc53b28669cf7c907d47e8653c08c6a"]
+id_admins = ["U9b54d3a7343a2c3fb82d8bb76d7ba314", "U2cc53b28669cf7c907d47e8653c08c6a"]
 admin_count = len(id_admins)
 timeout_menit = 60  # Timeout pengguna menghubungi admin sejak terakhir chat
 
